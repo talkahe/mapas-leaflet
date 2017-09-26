123af91355a99b4f40c6689a56b0591f5c12e325
Jorge G <info@jorge.gd>
2017-09-26 12:32:21 +0200

    Mapa con geolocalizacion

--- mapa.js
+++ mapa.js
@@ -1,15 +1,11 @@
 // Mapa Alfacar 1
-var map = L.map('mapa-alfacar', {
-	dragging: false,
-	scrollWheelZoom: false,
-	keyboard: false,
-	doubleClickZoom: false,
-	zoomControl: false
-}).setView([37.242331, -3.562764], 15);
+var map = L.map('mapa-alfacar').setView([40.25, -3.41], 6);
 
 L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
 	maxZoom: 18
 }).addTo(map);
 
-var markerFuenteGrande = L.marker([37.246660, -3.553813],{draggable: false, title: 'Fuente Grande'}).addTo(map);
-markerFuenteGrande.bindTooltip("Fuente Grande").openTooltip();
+map.locate({setView: true});
+map.on('locationfound', function(e){
+	L.marker(e.latlng).addTo(map);
+});
