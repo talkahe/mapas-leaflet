47a7bf0493869d4e4815c27ac1b2872a15bc003a
Jorge G <info@jorge.gd>
2017-10-18 08:43:44 +0200

    Cambiada capa del mapa

--- js/mapa.js
+++ js/mapa.js
@@ -1,9 +1,9 @@
 window.onload  = function(){
-	// Mapa Alfacar 1
-	var map = L.map('mapa-alfacar').setView([40.25, -3.41], 6);
+	var map = L.map('geomapa');
 
-	L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-		maxZoom: 18
+	L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+		maxZoom: 20,
+		subdomains:['mt0','mt1','mt2','mt3']
 	}).addTo(map);
 
 	map.locate({setView: true});
